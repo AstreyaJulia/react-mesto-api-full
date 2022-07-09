c68088873a082613ce37dd6f57def5c4e0f066d2
Юлия Латышева <58434576+AstreyaJulia@users.noreply.github.com>
2022-07-09 21:26:58 +0300

    init

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -2,7 +2,7 @@
  * @type {{headers: {"Content-Type": string}, serverURL: string}}
  */
 export const apiSettings = {
-    serverURL: "http://api.julialatysheva.nomorepartiesxyz.ru",
+    serverURL: "https://api.julialatysheva.nomorepartiesxyz.ru",
     headers: {
         "Content-Type": "application/json",
         authorization: 'Bearer ' + localStorage.getItem("jwt"),
@@ -13,4 +13,4 @@ export const apiSettings = {
 /** Адрес сервера авторизации
  * @type {string}
  */
-export const BASE_URL = "http://api.julialatysheva.nomorepartiesxyz.ru";
+export const BASE_URL = "https://api.julialatysheva.nomorepartiesxyz.ru";
